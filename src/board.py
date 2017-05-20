6a89132c9ac46df9e172b1a400026a9a07d8b7c9
lore <lorenzomartino86@gmail.com>
2017-05-20 23:56:20 +0200

    added grid of correspondences

--- src/board.py
+++ src/board.py
@@ -4,7 +4,7 @@ from src.helper import Helper
 
 class Board(object):
 
-    def __init__(self):
+    def __init__(self, grid=None):
         self.rows = 'ABCDEFGHI'
         self.columns = '123456789'
         self.placeholder = '.'
@@ -14,19 +14,28 @@ class Board(object):
         self.square_units = [Helper.cross(row_square, column_square)
                              for row_square in ('ABC', 'DEF', 'GHI')
                              for column_square in ('123', '456', '789')]
+        self._build_grid(grid)
 
 
-    def grid_values(self, grid=None):
-        self.grid_dict = {}
-        assert grid is not None, "grid should be defined"
-        assert len(grid) is 81, "grid should have 81 digits"
-        item_index = 0
-        for item in grid:
-            self._validate(item)
-            box = self.boxes[item_index]
-            self.grid_dict[box] = item
-            item_index = item_index + 1
-        return self.grid_dict
+    def grid_values(self):
+        """
+        Returns:
+            Sudoku grid with corresponding boxes in dictionary form:
+            - keys: Box labels, e.g. 'A1'
+            - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+        """
+        all_digits = '123456789'
+        self.grid_correspondences = {}
+        for key in self.grid_dict:
+            value = self.grid_dict[key]
+            if value == self.placeholder:
+                self.grid_correspondences[key] = all_digits
+            else:
+                self.grid_correspondences[key] = value
+        return self.grid_correspondences
+
+
+
 
     def show(self):
         """
@@ -42,10 +51,6 @@ class Board(object):
             if r in 'CF': print(line)
         return
 
-    def _validate(self, item):
-        if item not in (str(value) for value in range(1, 10)) and item != self.placeholder:
-            raise ValueError("grid contains a not valid value: " + item)
-
     def get_boxes(self):
         return self.boxes
 
@@ -57,3 +62,27 @@ class Board(object):
 
     def get_square_units(self):
         return self.square_units
+
+    def _build_grid(self, grid):
+        """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
+
+               Args:
+                   grid: Sudoku grid in string form, 81 characters long
+               Returns:
+                   Sudoku grid in dictionary form:
+                   - keys: Box labels, e.g. 'A1'
+                   - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+               """
+        self.grid_dict = {}
+        assert grid is not None, "grid should be defined"
+        assert len(grid) is 81, "grid should have 81 digits"
+        item_index = 0
+        for item in grid:
+            self._validate(item)
+            box = self.boxes[item_index]
+            self.grid_dict[box] = item
+            item_index = item_index + 1
+
+    def _validate(self, item):
+        if item not in (str(value) for value in range(1, 10)) and item != self.placeholder:
+            raise ValueError("grid contains a not valid value: " + item)
--- test/test_board.py
+++ test/test_board.py
@@ -6,30 +6,37 @@ from src.board import Board
 
 class TestBoard(TestCase):
 
-    def setUp(self):
-        self.board = Board();
 
     def test_not_grid(self):
         with self.assertRaises(AssertionError):
-            self.board.grid_values()
+            board = Board()
 
     def test_too_long_grid(self):
         grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......' \
                       '8..67.82....26.95..8..2.3..9..5.1.3..' \
                       '........'
         with self.assertRaises(AssertionError):
-            self.board.grid_values(grid)
+            Board(grid)
 
     def test_wrong_placeholder_grid(self):
         grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......' \
                       '8..67.82....26.95..8..2.3..9..5.1.3.*'
         with self.assertRaises(ValueError):
-            self.board.grid_values(grid)
+            Board(grid)
 
     def test_correct_grid(self):
         grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......' \
                       '8..67.82....26.95..8..2.3..9..5.1.3..'
-        self.board.grid_values(grid)
+        board = Board(grid)
+
+    def test_correspondences_grid(self):
+        grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......' \
+                      '8..67.82....26.95..8..2.3..9..5.1.3..'
+        board = Board(grid)
+        correspondences = board.grid_values()
+        self.assertEqual(correspondences['A1'], '123456789')
+        self.assertEqual(correspondences['A3'], '3')
+
 
 if __name__ == '__main__':
     unittest.main()
